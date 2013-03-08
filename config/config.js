e2866733153f43487a780d829900506cc63275c2
Khalid Rahmani <khalid.rahmani.mail@gmail.com>
2013-03-08 15:39:06 +0000

    config

--- config/config.js
+++ config/config.js
@@ -1,60 +1,60 @@
 
 module.exports = {
-    development: {
-      root: require('path').normalize(__dirname + '/..'),
-      app: {
-        name: 'Nodejs Express Mongoose Demo'
-      },
-      db: 'mongodb://localhost/moneycercle1',
-      facebook: {
-          clientID: "APP_ID"
-        , clientSecret: "APP_SECRET"
-        , callbackURL: "http://localhost:3000/auth/facebook/callback"
-      },
-      twitter: {
-          clientID: "CONSUMER_KEY"
-        , clientSecret: "CONSUMER_SECRET"
-        , callbackURL: "http://localhost:3000/auth/twitter/callback"
-      },
-      github: {
-          clientID: 'APP_ID'
-        , clientSecret: 'APP_SECRET'
-        , callbackURL: 'http://localhost:3000/auth/github/callback'
-      },
-      google: {
-          clientID: "APP_ID"
-        , clientSecret: "APP_SECRET"
-        , callbackURL: "http://localhost:3000/auth/google/callback"
-      }
-    }
-  , test: {
-
-    }
-  , production: {
-	  root: require('path').normalize(__dirname + '/..'),
-      app: {
-          name: 'Nodejs Express Mongoose Demo'
-        },
-	  db: 'mongodb://heroku:8e85fbf681f6b48902442e03c2e7ff39@linus.mongohq.com:10089/app13037458',
-	  facebook: {
-          clientID: "APP_ID"
-        , clientSecret: "APP_SECRET"
-        , callbackURL: "http://localhost:3000/auth/facebook/callback"
-      },
-      twitter: {
-          clientID: "CONSUMER_KEY"
-        , clientSecret: "CONSUMER_SECRET"
-        , callbackURL: "http://localhost:3000/auth/twitter/callback"
-      },
-      github: {
-          clientID: 'APP_ID'
-        , clientSecret: 'APP_SECRET'
-        , callbackURL: 'http://localhost:3000/auth/github/callback'
-      },
-      google: {
-          clientID: "APP_ID"
-        , clientSecret: "APP_SECRET"
-        , callbackURL: "http://localhost:3000/auth/google/callback"
-      }
-    }
+	    development: {
+	      root: require('path').normalize(__dirname + '/..'),
+	      app: {
+	        name: 'Nodejs Express Mongoose Demo'
+	      },
+	      db: 'mongodb://localhost/moneycercle1',
+	      facebook: {
+	          clientID: "APP_ID"
+	        , clientSecret: "APP_SECRET"
+	        , callbackURL: "http://localhost:3000/auth/facebook/callback"
+	      },
+	      twitter: {
+	          clientID: "CONSUMER_KEY"
+	        , clientSecret: "CONSUMER_SECRET"
+	        , callbackURL: "http://localhost:3000/auth/twitter/callback"
+	      },
+	      github: {
+	          clientID: 'APP_ID'
+	        , clientSecret: 'APP_SECRET'
+	        , callbackURL: 'http://localhost:3000/auth/github/callback'
+	      },
+	      google: {
+	          clientID: "APP_ID"
+	        , clientSecret: "APP_SECRET"
+	        , callbackURL: "http://localhost:3000/auth/google/callback"
+	      }
+	    }
+	  , test: {
+	
+	    }
+	  , production: {
+		  root: require('path').normalize(__dirname + '/..'),
+	      app: {
+	          name: 'Nodejs Express Mongoose Demo'
+	        },
+		  db: process.env.MONGOHQ_URL,
+		  facebook: {
+	          clientID: "APP_ID"
+	        , clientSecret: "APP_SECRET"
+	        , callbackURL: "http://localhost:3000/auth/facebook/callback"
+	      },
+	      twitter: {
+	          clientID: "CONSUMER_KEY"
+	        , clientSecret: "CONSUMER_SECRET"
+	        , callbackURL: "http://localhost:3000/auth/twitter/callback"
+	      },
+	      github: {
+	          clientID: 'APP_ID'
+	        , clientSecret: 'APP_SECRET'
+	        , callbackURL: 'http://localhost:3000/auth/github/callback'
+	      },
+	      google: {
+	          clientID: "APP_ID"
+	        , clientSecret: "APP_SECRET"
+	        , callbackURL: "http://localhost:3000/auth/google/callback"
+	      }
+	    }
 }
