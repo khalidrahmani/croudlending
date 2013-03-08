4cadbedb6682991e9728fe8973fe8658bae2b145
Khalid Rahmani <khalid.rahmani.mail@gmail.com>
2013-03-08 12:38:53 +0000

    config

--- config/config.js
+++ config/config.js
@@ -33,5 +33,25 @@ module.exports = {
   , production: {
 	  root: require('path').normalize(__dirname + '/..'),
 	  db: 'mongodb://heroku:8e85fbf681f6b48902442e03c2e7ff39@linus.mongohq.com:10089/app13037458'
+	  facebook: {
+          clientID: "APP_ID"
+        , clientSecret: "APP_SECRET"
+        , callbackURL: "http://localhost:3000/auth/facebook/callback"
+      },
+      twitter: {
+          clientID: "CONSUMER_KEY"
+        , clientSecret: "CONSUMER_SECRET"
+        , callbackURL: "http://localhost:3000/auth/twitter/callback"
+      },
+      github: {
+          clientID: 'APP_ID'
+        , clientSecret: 'APP_SECRET'
+        , callbackURL: 'http://localhost:3000/auth/github/callback'
+      },
+      google: {
+          clientID: "APP_ID"
+        , clientSecret: "APP_SECRET"
+        , callbackURL: "http://localhost:3000/auth/google/callback"
+      }
     }
 }
